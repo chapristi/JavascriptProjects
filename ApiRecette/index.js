dae5caea87ca007ff5672659e8f8f248fdd6f62f
chapristi <70172942+chapristi@users.noreply.github.com>
2021-12-05 20:24:26 +0100

    Create index.js

---
+++ ApiRecette/index.js
@@ -0,0 +1,46 @@
+const search = document.getElementById("search")
+const result  = document.getElementById("result")
+
+search.addEventListener("keyup", ({key}) => {
+    let searchValue = search.value
+    if (searchValue != ""){
+      if(key === "Enter"){
+        callApi(searchValue)
+      }
+
+    }else{
+      console.log("vous ne pouvez point entrer une valeur null-")
+    }
+})
+
+
+callApi = async (param) => {
+  await axios.get('https://www.themealdb.com/api/json/v1/1/search.php?s='+ param)
+  .then(function (response) {
+      if(response.data !==  null){
+    
+        FormatData(response.data.meals)
+      }
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    console.log("nous traitons votre demande")
+  });
+}
+
+
+FormatData = (data) => {
+
+    result.innerHTML =   data.map((datas) => 
+      `
+      <h2>${datas.strMeal}</h2>
+      <p>${datas.strInstructions}</p>
+      <ul>
+        <li>${datas.strIngredient1}</li>
+      </ul>
+      `
+    ).join("")
+  }
